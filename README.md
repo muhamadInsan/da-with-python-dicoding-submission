70fd1256b60405152292a7bd6040c6eb59134081
muhamadInsan <muhamad.insan.r@gmail.com>
2024-01-25 16:00:05 +0700

    update md file

--- README.md
+++ README.md
@@ -1,14 +1,35 @@
-# Dashboard ✨
+# diCommerce Dashboard
+A dashboard to visualize the e-commerce public datasets. The dashboard was created to fulfill the requirements submitted at the [dicoding.com](https://www.dicoding.com/academies/555/corridor) courses.
 
-## Setup environment
+## What information will you get from the dashboard
+
+ - Visualize the states and cities of the customer
+ - Product categories with the largest order
+ - Score review of each of the product categories
+ - Visualize of order statuses
+ - Visualize of RFM (Recency, Frequency, Monetary) analysis 
+
+## Prerequisite
+- Python (pandas, jupyter, plotly)
+- Streamlit (installation, deployment)
+
+## How to run
+### Setup Environment
+I use pip to set up my environment (env), please use set up env that makes you easier.
+
 ```
-conda create --name main-ds python=3.9
-conda activate main-ds
-pip install numpy pandas scipy matplotlib seaborn jupyter streamlit babel
+pyhton -m venv <env-name>
+<env-name>\scripts\activate
+
+pip install -r requirements.txt
 ```
 
-## Run steamlit app
+### Run steamlit app
 ```
+cd dashboard
 streamlit run dashboard.py
 ```
+Enjoy !!
+#
 
+Written by me with [StackEdit](https://stackedit.io/).
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,9 +6,22 @@ import streamlit as st
 import plotly.express as px
 sns.set(style='dark')
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    # page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
 # Load Data 
-all_data = pd.read_csv('../data/all_dataset.csv')
+all_data = pd.read_csv('../data/all_dataset.csv',
+                       sep=',',
+                       parse_dates=['order_purchase_timestamp'])
 
 st.title("diCommerce Dashboard")
 
@@ -20,7 +33,62 @@ with st.sidebar:
 tab1, tab2, tab3 = st.tabs(["Customer", "Product", "Order"])
 
 with tab1:
-    # Tab 1 Customer
+
+    # Figure Product RFM
+    df = all_data.groupby(by='customer_id', as_index=False).agg({
+        'order_purchase_timestamp':'max',
+        'order_id':'count',
+        'total_price':'sum'
+    })
+
+    df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
+    df["max_order_timestamp"] = df.max_order_timestamp.dt.date
+    rcnt_date = all_data.order_purchase_timestamp.dt.date.max()
+    df["recency"] = df["max_order_timestamp"].apply(lambda x: (rcnt_date - x).days)
+    df['cust_id_short'] = df.customer_id.str.slice(start=25)
+
+    df.drop("max_order_timestamp", axis=1, inplace=True)
+
+    st.subheader('Best Customer Based on RFM Parameters (customer_id)')
+
+    col1, col2, col3 = st.columns(3, gap='medium')
+
+    with col1:
+        
+        fig_recency = px.bar(
+            df.sort_values(by="recency", ascending=True).head(), 
+            y='recency',
+            x='cust_id_short',
+            title='By Recency (days)'
+            )
+        fig_recency.show()
+
+        st.plotly_chart(fig_recency, theme="streamlit", use_container_width=True)
+
+    with col2:
+        
+        fig_frequency = px.bar(
+            df.sort_values(by="frequency", ascending=False).head(), 
+            y='frequency',
+            x='cust_id_short',
+            title='By Frequency'
+            )
+        fig_frequency.show()
+
+        st.plotly_chart(fig_frequency, theme="streamlit", use_container_width=True)
+
+    with col3:
+        
+        fig_monetary = px.bar(
+            df.sort_values(by="monetary", ascending=False).head(), 
+            y='monetary',
+            x='cust_id_short',
+            title='By Monetary'
+            )
+        fig_monetary.show()
+
+        st.plotly_chart(fig_monetary, theme="streamlit", use_container_width=True)
+
     # cust by states
     custby_states = all_data.groupby(
                         by=['customer_state']).customer_id.nunique().sort_values(
@@ -65,7 +133,7 @@ with tab2:
         top_product_cat.head(10), 
         x='jumlah',
         y='product_category_name_english',
-        title='Top 10 Cities With The Largest Customer',
+        title='Top 10 Products With The Largest Order',
         orientation='h',
         )
     fig_top_product_cat.update_layout(yaxis={'categoryorder':'total ascending'})
@@ -78,3 +146,41 @@ with tab2:
 
     with col2:
         st.dataframe(top_product_cat.iloc[11:,], hide_index=False)
+
+
+    # Figure Product Review 
+    product = all_data[['review_id',
+                        'review_score', 
+                        'review_comment_title', 
+                        'review_comment_message',
+                        'review_creation_date',
+                        'product_category_name_english']]
+
+    product_review = product.groupby(by=['review_score','product_category_name_english']).agg({'review_id':'count'}).sort_values(
+        by='review_id', 
+        ascending=False).rename(columns={'review_id':'jumlah'}).reset_index()
+        
+    fig_product_review = px.treemap(product_review, 
+                                    path=[px.Constant("Product Score Review"), 'review_score', 'product_category_name_english'], 
+                                    values='jumlah', 
+                                    title='The Best Category Product Review')
+    fig_product_review.update_traces(root_color="grey")
+    fig_product_review.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+    fig_product_review.show()
+
+    st.plotly_chart(fig_product_review, use_container_width=True)
+
+with tab3:
+    order = all_data[['order_id', 'customer_id', 'order_status']]
+    status_order = order.groupby(by='order_status').agg({'order_id':'count'}).sort_values(
+        by='order_id', 
+        ascending=False).rename(columns={'order_id':'jml'}).reset_index()
+    
+    fig_status_order = px.pie(status_order, 
+                              values='jml', 
+                              names='order_status', 
+                              color_discrete_sequence=px.colors.sequential.RdBu,
+                              title='Order Status')
+    fig_status_order.show()
+
+    st.plotly_chart(fig_status_order, use_container_width=True)
